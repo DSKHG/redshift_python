3ca64673f2f990f5e9557b4c2d546544978cdd53
DSKHG <kumar.ravi.ec81@gmail.com>
2021-08-31 12:31:42 +0530

    Update and rename redshift_sqlalchemy_pandas_dataframe to redshift_sqlalchemy_pandas_dataframe.py

--- redshift_sqlalchemy_pandas_dataframe.py
+++ redshift_sqlalchemy_pandas_dataframe.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine,select,Table,Text
 import pandas as pd
+import psycopg2
 
 sql ="select * from dummy_table"
 engine= create_engine('redshift+psycopg2://username@host.amazonaws.com:5439/database')
